d86b20e8c94da25bfa4bfb104efa8a1ebbba16b1
Akmal Nizam <akmal.nizam@blueware.com.my>
2023-09-06 16:33:41 +0800

    Added timing

--- ObjectDetection.js
+++ ObjectDetection.js
@@ -1,38 +1,41 @@
-// declare global variable
+// declare global variables
 let video = null; // video element
 let detector = null; // detector object
 let detections = []; // store detection result
 let videoVisibility = true;
 let detecting = false;
+let detectionDelay = 1; // Delay in milliseconds between detections
 
-// global HTML element
-const toggleVideoEl = document.getElementById('toggleVideoEl');
-const toggleDetectingEl = document.getElementById('toggleDetectingEl');
+// global HTML elements
+const toggleVideoEl = document.getElementById("toggleVideoEl");
+const toggleDetectingEl = document.getElementById("toggleDetectingEl");
 
-// set cursor to wait until video elment is loaded
-document.body.style.cursor = 'wait';
+// set cursor to wait until video element is loaded
+document.body.style.cursor = "wait";
 
 // The preload() function if existed is called before the setup() function
 function preload() {
   // create detector object from "cocossd" model
-  detector = ml5.objectDetector('cocossd');
-  console.log('detector object is loaded');
+  detector = ml5.objectDetector("cocossd");
+  console.log("detector object is loaded");
 }
 
 // The setup() function is called once when the program starts.
 function setup() {
-  // create canvas element with 640 width and 480 height in pixel
+  // create canvas element with 640 width and 480 height in pixels
   createCanvas(640, 480);
   // Creates a new HTML5 <video> element that contains the audio/video feed from a webcam.
   // The element is separate from the canvas and is displayed by default.
   video = createCapture(VIDEO);
   video.size(640, 480);
-  console.log('video element is created');
-  video.elt.addEventListener('loadeddata', function() {
+  console.log("video element is created");
+  video.elt.addEventListener("loadeddata", function () {
     // set cursor back to default
     if (video.elt.readyState >= 2) {
-      document.body.style.cursor = 'default';
-      console.log('video element is ready! Click "Start Detecting" to see the magic!');
+      document.body.style.cursor = "default";
+      console.log(
+        'video element is ready! Click "Start Detecting" to see the magic!'
+      );
     }
   });
 }
@@ -48,17 +51,6 @@ function draw() {
   }
 }
 
-/*
-Exaple of an detect object
-{
-    "label": "person",
-    "confidence": 0.8013999462127686,
-    "x": 7.126655578613281,
-    "y": 148.3782720565796,
-    "width": 617.7880859375,
-    "height": 331.60210132598877,
-}
-*/
 function drawResult(object) {
   drawBoundingBox(object);
   drawLabel(object);
@@ -67,13 +59,13 @@ function drawResult(object) {
 // draw bounding box around the detected object
 function drawBoundingBox(object) {
   // Sets the color used to draw lines.
-  stroke('green');
+  stroke("green");
   // width of the stroke
   strokeWeight(4);
   // Disables filling geometry
   noFill();
-  // draw an rectangle
-  // x and y are the coordinates of upper-left corner, followed by width and height
+  // draw a rectangle
+  // x and y are the coordinates of the upper-left corner, followed by width and height
   rect(object.x, object.y, object.width, object.height);
 }
 
@@ -82,28 +74,28 @@ function drawLabel(object) {
   // Disables drawing the stroke
   noStroke();
   // sets the color used to fill shapes
-  fill('white');
+  fill("white");
   // set font size
   textSize(24);
   // draw string to canvas
   text(object.label, object.x + 10, object.y + 24);
 }
 
-// callback function. it is called when object is detected
+// callback function. it is called when an object is detected
 function onDetected(error, results) {
   if (error) {
     console.error(error);
   }
   detections = results;
-  // keep detecting object
+  // keep detecting objects with a delay
   if (detecting) {
-    detect(); 
+    setTimeout(detect, detectionDelay);
   }
 }
 
 function detect() {
-  // instruct "detector" object to start detect object from video element
-  // and "onDetected" function is called when object is detected
+  // instruct "detector" object to start detecting objects from the video element
+  // and "onDetected" function is called when an object is detected
   detector.detect(video, onDetected);
 }
 
@@ -111,10 +103,10 @@ function toggleVideo() {
   if (!video) return;
   if (videoVisibility) {
     video.hide();
-    toggleVideoEl.innerText = 'Show Video';
+    toggleVideoEl.innerText = "Show Video";
   } else {
     video.show();
-    toggleVideoEl.innerText = 'Hide Video';
+    toggleVideoEl.innerText = "Hide Video";
   }
   videoVisibility = !videoVisibility;
 }
@@ -123,9 +115,9 @@ function toggleDetecting() {
   if (!video || !detector) return;
   if (!detecting) {
     detect();
-    toggleDetectingEl.innerText = 'Stop Detecting';
+    toggleDetectingEl.innerText = "Stop Detecting";
   } else {
-    toggleDetectingEl.innerText = 'Start Detecting';
+    toggleDetectingEl.innerText = "Start Detecting";
   }
   detecting = !detecting;
 }
